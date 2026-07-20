f5d203d524767f96a2fdc93cc987e0441cfd28b3
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-07-20 12:16:11 +0545

    adding the hice

---
+++ flutter_Sajilo_Stay/sajilo_stay/lib/core/services/hive/hive_service.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sajilo_stay/core/constants/hive_table_constant.dart';
+import 'package:sajilo_stay/features/auth/data/models/auth_hive_model.dart';
+
+final hiveServiceProvider = Provider<HiveService>((ref) {
+  return HiveService();
+});
+
+class HiveService {
+  Future<void> init() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = '${directory.path}/${HiveTableConstant.dbName}';
+    Hive.init(path);
+    _registerAdapter();
+    await _openBoxes();
+  }
+
+  void _registerAdapter() {
+    if (!Hive.isAdapterRegistered(HiveTableConstant.authTypeId)) {
+      Hive.registerAdapter(AuthHiveModelAdapter());
+    }
+  }
+
+  Future<void> _openBoxes() async {
+    await Hive.openBox<AuthHiveModel>(HiveTableConstant.authTableName);
+  }
+
+  Future<void> close() async {
+    await Hive.close();
+  }
+
+  Box<AuthHiveModel> get _authBox =>
+      Hive.box<AuthHiveModel>(HiveTableConstant.authTableName);
+
+  Future<AuthHiveModel> registerUser(AuthHiveModel model) async {
+    await _authBox.put(model.userId, model);
+    return model;
+  }
+
+  Future<AuthHiveModel?> loginUser(String email, String password) async {
+    final users = _authBox.values.where(
+      (user) => user.email == email && user.password == password,
+    );
+    if (users.isNotEmpty) {
+      return users.first;
+    }
+    return null;
+  }
+}
