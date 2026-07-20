583d51d325da538f215b9b050fe661b9d0dc254c
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-07-20 12:14:08 +0545

    adding the error template

---
+++ flutter_Sajilo_Stay/sajilo_stay/lib/core/error/failures.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  final String message;
+  const Failure(this.message);
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class LocalDatabaseFailure extends Failure {
+  const LocalDatabaseFailure({String message = "Local Database Failure"})
+    : super(message);
+}
+
+class ServerFailure extends Failure {
+  final int? statusCode;
+  const ServerFailure({required String message, this.statusCode})
+    : super(message);
+
+  @override
+  List<Object?> get props => [message, statusCode];
+}
+
+class NetworkFailure extends Failure {
+  const NetworkFailure({String message = "No internet connection"})
+    : super(message);
+}
+
+class CacheFailure extends Failure {
+  const CacheFailure({String message = "Cache data not available"})
+    : super(message);
+}
